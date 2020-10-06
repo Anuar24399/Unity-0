e7202d344035b2ce4db560b400ecefd0cd8b9fe7
Anuar24399 <43638208+Anuar24399@users.noreply.github.com>
2020-10-06 09:58:19 +0200

    Add files via upload
    Unity-0 codes

---
+++ Grounded.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+
+
+    void Start()
+    {
+        Player = gameObject;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<PlayerController>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Player.GetComponent<PlayerController>().isGrounded = false;
+        }
+    }
+
+}
